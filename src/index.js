726c009149f1e7050e0d8339b0f8a78904b8bce4
ElitaGomez <ecgomezr@gmail.com>
2019-06-17 13:11:52 -0500

    actualizaciones finales en README y estilos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+// Llamando los ID de las cajas de texto y boton de ingreso
+
 const inicio = document.getElementById("login");
 const reporte = document.getElementById("cipher");
 const ingresar = document.getElementById("next");
 
 let contador = 0;
 
-// creando CONTRASEÑA: LABORATORIA
+// creando la CONTRASEÑA: LABORATORIA para ingresar y los mensajes de error
+
 ingresar.addEventListener("click", (event) => {
   event.preventDefault();
   const contraseña = document.getElementById("password").value;
@@ -22,28 +25,27 @@ ingresar.addEventListener("click", (event) => {
   }
  });
 
-
-
-// cifrando el mensaje 
+// llamando a los ID de los botones de cifrado y descifrado 
 
 const btncifrar = document.getElementById("encode");
 const btndescifrar = document.getElementById("decode");
 
-
+// llamando al boton cifrar 
 btncifrar.addEventListener("click", () => {
   let string = document.getElementById("message").value.toUpperCase();
-  let offset = parseInt(document.getElementById("clave").value);
+  let offset = parseInt(document.getElementById("clave").value); // value : captura el valor ingresado en la caja
   document.getElementById("result").innerHTML = cipher.encode(offset, string);
 });
 
-
+// llamando al boton descifrar
 btndescifrar.addEventListener("click", () => {
   let string = document.getElementById("message").value;
-  let offset = parseInt(document.getElementById("clave").value);
+  let offset = parseInt(document.getElementById("clave").value); 
   document.getElementById("result").innerHTML = cipher.decode(offset, string);
 
 });
 
+// llamando al boton limpiar
 document.getElementById("clean").addEventListener("click", (event) => {
   event.preventDefault
   document.getElementById("result").innerHTML = null;
