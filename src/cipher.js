41eb9ff9bc5b35553b63e4d49891c3a4fb814f96
ElitaGomez <ecgomezr@gmail.com>
2019-06-21 13:07:24 -0500

    actualizando cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,16 @@
 window.cipher = {
   encode: (offset, string) => {
 
-    let cifra = "";  // la variable let nos servira para almacenar el valor del string cifrado
-    for (let i = 0; i < string.length; i++) {  // para recorrer todo el string del texto
+    let cifra = "";  
+    for (let i = 0; i < string.length; i++) {  
       let numascii = string.charCodeAt(i);
-      if (65 <= numascii && numascii <= 90) {  // valor de las letras mayusculas en ASCII
-        cifra += String.fromCharCode((numascii - 65 + offset) % 26 + 65); // formar la cadena del texto con el valor obtenido luego de aplicar formula
-      } else if (numascii === 32) {  // para verificar si es un espacio vacio
-        cifra += " ";  // añadir espacio en string cifrado
+      if (65 <= numascii && numascii <= 90) {  
+        cifra += String.fromCharCode((numascii - 65 + offset) % 26 + 65); 
+      } else if (numascii === 32) {  
+        cifra += " ";  
       }
     }
-    return cifra; //retorna el valor de la cadena cifrada
+    return cifra; 
   },
 
   decode: (offset, string) => {
--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@
 const inicio = document.getElementById("login");
 const reporte = document.getElementById("cipher");
 const ingresar = document.getElementById("next");
-
+const btncifrar = document.getElementById("encode");
+const btndescifrar = document.getElementById("decode");
 let contador = 0;
 
-// creando la CONTRASEÑA: LABORATORIA para ingresar y los mensajes de error
-
+// creando la CONTRASEÑA: LABORATORIA
 ingresar.addEventListener("click", (event) => {
   event.preventDefault();
   const contraseña = document.getElementById("password").value;
   if (contraseña === "LABORATORIA") {
     inicio.classList.add("hide");
     reporte.classList.remove("hide");
-  }
-  else if
-    (contador == 3) {
+  }else if(contador == 3) {
     document.getElementById("error").innerHTML = "Ya utilizaste todos tus intentos, en este momento no podrás ingresar";
   }
   else {
@@ -25,21 +23,16 @@ ingresar.addEventListener("click", (event) => {
   }
 });
 
-// llamando a los ID de los botones de cifrado y descifrado 
-
-const btncifrar = document.getElementById("encode");
-const btndescifrar = document.getElementById("decode");
-
 // llamando al boton cifrar 
 btncifrar.addEventListener("click", () => {
-  let string = document.getElementById("message").value.toUpperCase(); // toUppercase: para devolver el valor en mayusculas.
-  let offset = parseInt(document.getElementById("clave").value); // value : captura el valor ingresado en la caja
-  document.getElementById("result").innerHTML = cipher.encode(offset, string);//parseInt lo que hace es analizar una cadena de texto y retornar el valor numérico.
+  let string = document.getElementById("message").value.toUpperCase(); 
+  let offset = parseInt(document.getElementById("clave").value); 
+  document.getElementById("result").innerHTML = cipher.encode(offset, string);
 });
 
 // llamando al boton descifrar
 btndescifrar.addEventListener("click", () => {
-  let string = document.getElementById("message").value; // usamos let porque sabemos que nuestro elemento cambiarara segun el texto ingresado 
+  let string = document.getElementById("message").value; 
   let offset = parseInt(document.getElementById("clave").value);
   document.getElementById("result").innerHTML = cipher.decode(offset, string);
 
